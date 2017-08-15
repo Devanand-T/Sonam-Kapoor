53040fa59a3dd31a9fa06d5445e036dd0d0e84f5
Devanand-T <30316224+Devanand-T@users.noreply.github.com>
2017-08-15 16:04:57 +0530

    Update oddbet.c

--- oddbet.c
+++ oddbet.c
@@ -7,7 +7,6 @@ printf("Enter the minimum range: ");
 scanf("%d",&min);
 printf("Enter the maximum range: ");
 scanf("%d",&max);
-printf("Odd numbers in given range are: ");
 for(number = min;number <= max; number++)
 if(number % 2 !=0)
 printf("%d ",number);
