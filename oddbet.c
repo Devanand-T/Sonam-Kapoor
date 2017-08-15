43f21ce214d67cef0fc7f4dbaf343af2fec70b02
Devanand-T <30316224+Devanand-T@users.noreply.github.com>
2017-08-15 16:02:20 +0530

    Create oddbet.c

---
+++ oddbet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int number;
+int min,max;
+printf("Enter the minimum range: ");    
+scanf("%d",&min);
+printf("Enter the maximum range: ");
+scanf("%d",&max);
+printf("Odd numbers in given range are: ");
+for(number = min;number <= max; number++)
+if(number % 2 !=0)
+printf("%d ",number);
+}
